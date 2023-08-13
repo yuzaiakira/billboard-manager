fe845b193ba197f43b0d5f2e10cf974098afe554
yuzaiakira <yuzai.akira@gmail.com>
2023-08-13 21:56:38 +0330

    make user model

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserModel
 
 # Register your models here.
+
+
+@admin.register(UserModel)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("وضعیت", {'fields': ('user_group', 'display_name')}),
+    )
+    list_display = ['username', 'first_name', 'last_name', 'group', 'is_staff']
+    list_filter = ['group', 'is_staff']
--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class UserModel(AbstractUser):
+    user_group_status = (
+        (0, 'کاربر ساده'),
+        (1, 'نماینده'),
+        (2, 'ادمین'))
+    user_group = models.PositiveSmallIntegerField(choices=user_group_status, verbose_name="نوع کاربر", default=0)
+    display_name = models.CharField(max_length=50, verbose_name="نام نمایشی در سایت", blank=True, null=True)
