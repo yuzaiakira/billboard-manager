79cdc37087e27b497f2c7ff4ecfc3dd7678c1eb7
yuzaiakira <yuzai.akira@gmail.com>
2023-08-15 21:12:09 +0330

    add apps in INSTALLED_APPS and add AUTH_USER_MODEL variable for adding custom field in Django user model

--- core/settings.py
+++ core/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom apps
+    'adminpanel.apps.AdminpanelConfig',
+    'front.apps.FrontConfig',
+    'billbord.apps.BillbordConfig'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'adminpanel.UserModel'
