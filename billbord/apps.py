2026bd72aa6f30fcead48b93898ad4adcd450a02
yuzaiakira <yuzai.akira@gmail.com>
2023-08-15 21:20:22 +0330

    change language to persian

--- billbord/apps.py
+++ billbord/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BillbordConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'billbord'
+    verbose_name = 'مدیریت بیلبورد'
