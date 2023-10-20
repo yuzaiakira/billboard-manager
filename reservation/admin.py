90112ea902c58072c812fd43291ee3901593ec4f
yuzaiakira <yuzai.akira@gmail.com>
2023-10-20 20:48:41 +0330

    make add to reservation

--- reservation/admin.py
+++ reservation/admin.py
@@ -16,3 +16,10 @@ class RentalListAdmin(admin.ModelAdmin):
     list_filter = ('start_date', 'end_date')
     search_fields = ('contract', 'billboard', 'start_date', 'end_date')
     raw_id_fields = ('contract', 'billboard')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if 'billboard_id' in request.GET:
+            form.base_fields['billboard'].initial = request.GET['billboard_id']
+            form.base_fields['billboard'].widget.attrs['readonly'] = True
+        return form
