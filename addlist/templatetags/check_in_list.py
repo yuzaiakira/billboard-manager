edbc794c6d7e66a6290a2ce6adfd4f08757b8c5f
yuzaiakira <yuzai.akira@gmail.com>
2023-12-03 20:34:48 +0330

    Improving on_list template filter

--- addlist/templatetags/check_in_list.py
+++ addlist/templatetags/check_in_list.py
@@ -1,11 +1,16 @@
 from django import template
 from addlist.models import ListsModel
 register = template.Library()
+from django.contrib.auth.models import AnonymousUser
+
 
 @register.filter
 def on_list(value, user):
+    if isinstance(user, AnonymousUser):
+        # Return False or handle as you see appropriate when user is not authenticated
+        return False
     try:
-        ListsModel.objects.get(billboard_id=value.id, user= user)
+        value.ListModel.get(user=user)
         return True
     except ListsModel.DoesNotExist:
         return False
