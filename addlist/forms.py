57e8d83b4746afb1e81f52c4f77374f47fc73907
yuzaiakira <yuzai.akira@gmail.com>
2023-12-03 23:19:56 +0330

    filtring fields for get pdf

---
+++ addlist/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class ChoseFieldForm(forms.Form):
+    billboard_pic = forms.BooleanField(label="عکس بیلبورد", required=False, initial=True)
+    id_code = forms.BooleanField(label="کد بیلبورد", required=False)
+    name = forms.BooleanField(label="عنوان بیلبورد", required=False, initial=True)
+    description = forms.BooleanField(label="توضیحات بیلبورد", required=False)
+    address = forms.BooleanField(label="ادرس بیلبورد", required=False, initial=True)
+    has_power = forms.BooleanField(label="روشنایی بیلبورد", required=False, initial=True)
+    size = forms.BooleanField(label="سایز های بیلبورد", required=False, initial=True)
+    reservation_date = forms.BooleanField(label="تاریخ رزرو بیلبورد", required=False, initial=True)
+    price = forms.BooleanField(label="قیمت بیلبورد", required=False)
+
