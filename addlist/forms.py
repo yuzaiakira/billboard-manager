e7f562d937c684ee42041aee9059c5928d568677
unknown <yuzai.akira@gmail.com>
2024-09-15 11:43:51 +0330

    add branding in print billboard

--- addlist/forms.py
+++ addlist/forms.py
@@ -10,5 +10,6 @@ class ChoseFieldForm(forms.Form):
     has_power = forms.BooleanField(label="روشنایی بیلبورد", required=False, initial=True)
     size = forms.BooleanField(label="سایز های بیلبورد", required=False, initial=True)
     reservation_date = forms.BooleanField(label="تاریخ رزرو بیلبورد", required=False, initial=True)
+    brand = forms.BooleanField(label="نمایش برند", required=False, initial=True)
     price = forms.BooleanField(label="قیمت بیلبورد", required=False)
 
