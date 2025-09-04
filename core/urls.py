a9f5cf96127e5bb6aa0567ecc09bf53308de1fe2
Akira <yuzai.akira@gmail.com>
2025-09-04 11:19:41 +0330

    Add boolean value filter for template tags and update static file settings
    - Introduced a new template tag filter `boolean_value` to display "دارد" or "ندارد" based on boolean input.
    - Updated `settings.py` to change the static files directory to point to the actual static directory and adjusted `STATIC_ROOT` to avoid conflicts.
    - Enhanced `urls.py` to serve static files in production using `staticfiles_urlpatterns`.

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,11 @@ urlpatterns = [
     path('', include('billboard.urls'))
 ]
 
+# Serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Serve static files in production
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
